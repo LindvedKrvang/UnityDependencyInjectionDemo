cce94c4271307df5ba599b9110d153549e16f270
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2018-09-25 22:18:32 +0200

    Add project files.

---
+++ UnityDependencyInjectionDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace UnityDependencyInjectionDemo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ UnityDependencyInjectionDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UnityDependencyInjectionDemo.MvcApplication" Language="C#" %>
